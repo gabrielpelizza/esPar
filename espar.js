da11b5bc2afa0eb22f3e6a3ee99872f095d77a40
gabrielpelizza <57191488+gabrielpelizza@users.noreply.github.com>
2020-12-22 10:22:44 -0300

    Add files via upload

---
+++ espar.js
@@ -0,0 +1,10 @@
+function esPar(num){
+    if(num % 2 === 0){
+        console.log("Es par")
+    }else{
+        console.log("No es par")
+    };
+};
+
+console.log(esPar(6))
+console.log(esPar(7))
